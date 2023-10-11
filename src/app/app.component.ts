5d0cb3f9cacff1995de56579672abef319fa056d
Mirza Cerim <retro2115@outlook.com>
2023-10-11 22:28:34 +0200

    adding inital ngclass code to highlight active page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'directives';
 
+  currentPage = 0;
   images = [
+	
 	{
 		title: 'at the beach',
 		url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
