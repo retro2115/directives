b1428bac9cee3e08b09a4920f5057061149228ea
Mirza Cerim <retro2115@outlook.com>
2023-10-16 18:01:10 +0200

    code finished but needs checking for error

--- src/app/times.directive.ts
+++ src/app/times.directive.ts
@@ -16,7 +16,9 @@ export class TimesDirective {
 	this.viewContainer.clear();
 
 	for (let i = 0; i < times; i++) {
-		this.viewContainer.createEmbeddedView(this.templateRef, {});
+		this.viewContainer.createEmbeddedView(this.templateRef, {
+			images: i
+		});
 	}
    }
 }
