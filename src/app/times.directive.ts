466a444f87a951e2fe1f443371de9d0376edd0bb
Mirza Cerim <retro2115@outlook.com>
2023-10-16 17:44:30 +0200

    starting with a custom structural directive

---
+++ src/app/times.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appTimes]'
+})
+export class TimesDirective {
+
+  constructor(
+	private viewContainer: ViewContainerRef,
+	private templateRef: TemplateRef<any>
+  ) {
+	
+   }
+
+}
